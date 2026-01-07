0548d4a1d2490dd66d990a1e765ff55d52835291
Kalyani B Desale <103747210+Kalyani411@users.noreply.github.com>
2026-01-07 16:29:19 +0530

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,16 @@
+
 import { PrismaClient } from "@prisma/client";
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
-import 'dotenv/config';
 
-const databaseUrl = process.env.DATABASE_URL;
-if (!databaseUrl) {
-  throw new Error('Missing DATABASE_URL in .env');
-}
-const adapter=new PrismaMariaDb(databaseUrl);
-export const prisma = globalThis.prisma ?? new PrismaClient({ adapter, log: ['query','warn','error'] });
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-if (process.env.NODE_ENV !== 'production') {
-  (globalThis).prisma = prisma;
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
